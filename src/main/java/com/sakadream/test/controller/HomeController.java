885f9c250b724679a1bc8a08cfaee013bf71cf4e
SakaDream <bahaimt@gmail.com>
2017-03-24 18:51:22 +0700

    Add .gitignore

--- src/main/java/com/sakadream/test/controller/HomeController.java
+++ src/main/java/com/sakadream/test/controller/HomeController.java
@@ -1,9 +1,13 @@
 package com.sakadream.test.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 @Controller
 public class HomeController {
-    @RequestMapping("index")
-    public void index() {
+    @RequestMapping(value = "index", method = RequestMethod.GET)
+    public String index() {
         return "index";
     }
 }
