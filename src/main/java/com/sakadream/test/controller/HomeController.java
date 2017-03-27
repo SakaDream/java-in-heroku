36d8d7d428d6f7c859017046da0f07490b9d85ae
SakaDream <bahaimt@gmail.com>
2017-03-28 00:32:26 +0700

    Add check session

--- src/main/java/com/sakadream/test/controller/HomeController.java
+++ src/main/java/com/sakadream/test/controller/HomeController.java
@@ -14,7 +14,6 @@ import com.sakadream.test.model.Functions;;
 @Controller
 public class HomeController {
     Functions fn = new Functions();
-    HttpSession session;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
@@ -23,7 +22,7 @@ public class HomeController {
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public String login(@RequestParam("username") String username, @RequestParam("password") String password,
-            ModelMap model) throws Exception {
+            HttpSession session, ModelMap model) throws Exception {
         if (fn.checkLogin(username, password, session)) {
             model.addAttribute("list", fn.showAllEmployees());
             return "redirect:employees.htm";
@@ -34,54 +33,75 @@ public class HomeController {
     }
 
     @RequestMapping(value = "/employees", method = RequestMethod.GET)
-    public String employees(ModelMap model) throws Exception {
+    public String employees(HttpSession session, ModelMap model) throws Exception {
         if (fn.checkSession(session)) {
             model.addAttribute("list", fn.showAllEmployees());
             fn.cleanConnection();
             return "redirect:employees.htm";
         } else {
-            return "index";
+            return "redirect:index.htm";
         }
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
-    public String add() {
-        return "add";
+    public String add(HttpSession session) {
+        if (fn.checkSession(session)) {
+            return "redirect:add.htm";
+        } else {
+            return "redirect:index.htm";
+        }
     }
 
     @RequestMapping(value = "/add-post", method = RequestMethod.POST)
     public String addPost(@RequestParam("fullname") String fullName, @RequestParam("address") String address,
             @RequestParam("email") String email, @RequestParam("phone") String phone,
-            @RequestParam("salary") String salary, ModelMap model) throws Exception {
-        fn.add(new Employee(fullName, address, email, phone, Integer.valueOf(salary)));
-        model.addAttribute("list", fn.showAllEmployees());
-        return "redirect:employees.htm";
+            @RequestParam("salary") String salary, HttpSession session, ModelMap model) throws Exception {
+        if (fn.checkSession(session)) {
+            fn.add(new Employee(fullName, address, email, phone, Integer.valueOf(salary)));
+            model.addAttribute("list", fn.showAllEmployees());
+            return "redirect:employees.htm";
+        } else {
+            return "redirect:index.htm";
+        }
     }
 
     @RequestMapping(value = "/edit", method = RequestMethod.GET)
-    public String edit(@RequestParam("id") String id, ModelMap model) throws Exception {
-        try {
-            model.addAttribute("e", fn.getEmployee(Integer.valueOf(id)));
-            return "edit";
-        } catch (Exception e) {
-            model.addAttribute("list", fn.showAllEmployees());
-            return "employees";
+    public String edit(@RequestParam("id") String id, HttpSession session, ModelMap model) throws Exception {
+        if (fn.checkSession(session)) {
+            try {
+                model.addAttribute("e", fn.getEmployee(Integer.valueOf(id)));
+                return "edit";
+            } catch (Exception e) {
+                model.addAttribute("list", fn.showAllEmployees());
+                return "redirect:employees.htm";
+            }
+        } else {
+            return "redirect:index.htm";
         }
     }
 
     @RequestMapping(value = "/edit-post", method = RequestMethod.POST)
     public String editPost(@RequestParam("fullname") String fullName, @RequestParam("address") String address,
             @RequestParam("email") String email, @RequestParam("phone") String phone,
-            @RequestParam("salary") String salary, @RequestParam("id") String id, ModelMap model) throws Exception {
-        fn.edit(Integer.valueOf(id), new Employee(fullName, address, email, phone, Integer.valueOf(salary)));
-        model.addAttribute("list", fn.showAllEmployees());
-        return "redirect:employees.htm";
+            @RequestParam("salary") String salary, @RequestParam("id") String id, HttpSession session, ModelMap model)
+            throws Exception {
+        if (fn.checkSession(session)) {
+            fn.edit(Integer.valueOf(id), new Employee(fullName, address, email, phone, Integer.valueOf(salary)));
+            model.addAttribute("list", fn.showAllEmployees());
+            return "redirect:employees.htm";
+        } else {
+            return "redirect:index.htm";
+        }
     }
 
     @RequestMapping(value = "/delete", method = RequestMethod.GET)
-    public String delete(@RequestParam("id") String id, ModelMap model) throws Exception {
-        fn.delete(Integer.valueOf(id));
-        model.addAttribute("list", fn.showAllEmployees());
-        return "redirect:employees.htm";
+    public String delete(@RequestParam("id") String id, HttpSession session, ModelMap model) throws Exception {
+        if (fn.checkSession(session)) {
+            fn.delete(Integer.valueOf(id));
+            model.addAttribute("list", fn.showAllEmployees());
+            return "redirect:employees.htm";
+        } else {
+            return "redirect:index.htm";
+        }
     }
 }
